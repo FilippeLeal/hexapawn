e8d5faaa8509f3629769ef4589bf72d4c1b7919b
Filippe Leal <filippehleal@gmail.com>
2019-08-12 15:36:41 -0300

    added images for the pieces. Restart speed sligthly reduced

--- Pieces.py
+++ Pieces.py
@@ -48,7 +48,19 @@ class Pieces:
         return self.controlled
 
     def draw(self,screen,size,board,update=False):
-        center=( int(self.location[0]*size[0]/board+size[0]/2/board),int(self.location[1]*size[0]/board+size[0]/2/board) )
-        pygame.draw.circle(screen, self.color, center, int(size[0]/board/4))
+        squareSize=int(size[0]/board)
+        #center=( int(self.location[0]*size[0]/board+size[0]/2/board),int(self.location[1]*size[0]/board+size[0]/2/board) )
+        corner=( int(self.location[0]*size[0]/board ), int(self.location[1]*size[0]/board ))
+        bpawn = pygame.image.load('bpawn.png')
+        bpawn2 = pygame.transform.scale(bpawn, (int(size[0]/board),int(size[0]/board)))
+        wpawn = pygame.image.load('pawn.png')
+        wpawn2 = pygame.transform.scale(wpawn, (int(size[0]/board),int(size[0]/board)))
+        if self.color==(255,255,0):
+            pygame.draw.rect(screen,self.color,(int(self.location[0]*squareSize),int(self.location[1]*squareSize),squareSize,squareSize))
+        if self.controlled=="CPU":
+            screen.blit(bpawn2, corner)
+        elif self.controlled=="player":
+            screen.blit(wpawn2, corner)
+        #pygame.draw.circle(screen, self.color, center, int(size[0]/board/4))
         if update==True:
             pygame.display.update()
